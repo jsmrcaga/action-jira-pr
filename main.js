7b6b15006fa5a5a49de1a4e707872597a84dd08c
Jo Colina <jo.colina@weezevent.com>
2019-09-06 09:30:49 +0200

    GitHub actions: create own workflow

--- main.js
+++ main.js
@@ -2,12 +2,13 @@ const context = require('actions-context');
 const handlers = require('./lib/handlers');
 
 const { GITHUB_ACTION } = context.env;
+let event = context.json;
 
 if(GITHUB_ACTION !== 'pull_request') {
+	console.log(`[JIRA Action][Pull Request] Action (${GITHUB_ACTION}) is not pull request, ignoring`);
+	console.log(`Event received:\n`, event);
 	return process.exit(0);
 }
-
-let event = context.json;
 
 if(!handlers[event.action]) {
 	console.log(`[JIRA Action][Pull Request] No handler for event ${event.action}`);
