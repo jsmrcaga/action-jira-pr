79146c10260afc010c698fd1f48c0f0fdc978d07
Jo Colina <jo.colina@weezevent.com>
2019-09-09 17:22:02 +0200

    Handlers: only require when needed

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 const debug = require('./lib/debug');
 const context = require('actions-context');
-const handlers = require('./lib/handlers');
 
-const { GITHUB_ACTION } = context.env;
+const { GITHUB_EVENT_NAME } = context.env;
 let event = context.json;
 
-if(GITHUB_ACTION !== 'pull_request') {
-	console.log(`[JIRA Action][Pull Request] Action (${GITHUB_ACTION}) is not pull request, ignoring`);
+if(GITHUB_EVENT_NAME !== 'pull_request') {
+	console.log(`[JIRA Action][Pull Request] Action (${GITHUB_EVENT_NAME}) is not pull request, ignoring`);
 	debug(`Event received:\n`, event);
 	return process.exit(0);
 }
+
+const handlers = require('./lib/handlers');
 
 if(!handlers[event.action]) {
 	console.log(`[JIRA Action][Pull Request] No handler for event ${event.action}`);
