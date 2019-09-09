257dc656d5e921fc93ee0853160c7014c0189a00
Jo Colina <jo.colina@weezevent.com>
2019-09-09 18:02:43 +0200

    Debug: add event when not running

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ let event = context.json;
 
 if(GITHUB_EVENT_NAME !== 'pull_request') {
 	console.log(`[JIRA Action][Pull Request] Action (${GITHUB_EVENT_NAME}) is not pull request, ignoring`);
-	debug(`Event received:\n`, event);
+	debug(`Event name:\n`, GITHUB_EVENT_NAME);
 	return process.exit(0);
 }
 
@@ -14,7 +14,10 @@ const handlers = require('./lib/handlers');
 
 if(!handlers[event.action]) {
 	console.log(`[JIRA Action][Pull Request] No handler for event ${event.action}`);
+	debug(`Event received:\n`, event);
 	return process.exit(0);
 }
+
+const { env } = context;
 
 return handlers[event.action]({ event, env });
